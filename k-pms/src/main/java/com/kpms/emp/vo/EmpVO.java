1bb622a5d9bea892ef26c8c6a08c5d2ce4fbd6b2
JinbaeByeon <quswlsqo@gmail.com>
2023-04-06 17:28:59 +0900

    js,css,img 추가/ emp, lgntrycnt

---
+++ k-pms/src/main/java/com/kpms/emp/vo/EmpVO.java
@@ -0,0 +1,360 @@
+package com.kpms.emp.vo;
+
+/**
+ * EMP
+ */
+public class EmpVO {
+
+	/**
+	 * 
+	 */
+	private String empId;
+
+	/**
+	 * 
+	 */
+	private String crtDt;
+
+	/**
+	 * 
+	 */
+	private String crtr;
+
+	/**
+	 * 
+	 */
+	private String mdfyDt;
+
+	/**
+	 * 
+	 */
+	private String mdfyr;
+
+	/**
+	 * 
+	 */
+	private String useYn;
+
+	/**
+	 * 
+	 */
+	private String delYn;
+
+	/**
+	 * 
+	 */
+	private String nm;
+
+	/**
+	 * 
+	 */
+	private String brthdy;
+
+	/**
+	 * 
+	 */
+	private String eml;
+
+	/**
+	 * 
+	 */
+	private String phn;
+
+	/**
+	 * 
+	 */
+	private int pstnPrd;
+
+	/**
+	 * 
+	 */
+	private String prflPht;
+
+	/**
+	 * 
+	 */
+	private String hrDt;
+
+	/**
+	 * 
+	 */
+	private int hrPrd;
+
+	/**
+	 * 
+	 */
+	private String addrss;
+
+	/**
+	 * 
+	 */
+	private String emplmntStts;
+
+	/**
+	 * 
+	 */
+	private String admnYn;
+
+	/**
+	 * 
+	 */
+	private String pwdChngDt;
+
+	/**
+	 * 
+	 */
+	private String ltstLgnIp;
+
+	/**
+	 * 
+	 */
+	private String ltstLgnDt;
+
+	/**
+	 * 
+	 */
+	private int lgnCnt;
+
+	/**
+	 * 
+	 */
+	private String pwdSalt;
+
+	/**
+	 * 
+	 */
+	private String pwd;
+
+	/**
+	 * 
+	 */
+	private int pstnId;
+
+	/**
+	 * 
+	 */
+	private int jobId;
+
+	/**
+	 * 
+	 */
+	private String depId;
+
+
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpId() {
+		return this.empId;
+	}
+
+	public void setCrtDt(String crtDt) {
+		this.crtDt = crtDt;
+	}
+
+	public String getCrtDt() {
+		return this.crtDt;
+	}
+
+	public void setCrtr(String crtr) {
+		this.crtr = crtr;
+	}
+
+	public String getCrtr() {
+		return this.crtr;
+	}
+
+	public void setMdfyDt(String mdfyDt) {
+		this.mdfyDt = mdfyDt;
+	}
+
+	public String getMdfyDt() {
+		return this.mdfyDt;
+	}
+
+	public void setMdfyr(String mdfyr) {
+		this.mdfyr = mdfyr;
+	}
+
+	public String getMdfyr() {
+		return this.mdfyr;
+	}
+
+	public void setUseYn(String useYn) {
+		this.useYn = useYn;
+	}
+
+	public String getUseYn() {
+		return this.useYn;
+	}
+
+	public void setDelYn(String delYn) {
+		this.delYn = delYn;
+	}
+
+	public String getDelYn() {
+		return this.delYn;
+	}
+
+	public void setNm(String nm) {
+		this.nm = nm;
+	}
+
+	public String getNm() {
+		return this.nm;
+	}
+
+	public void setBrthdy(String brthdy) {
+		this.brthdy = brthdy;
+	}
+
+	public String getBrthdy() {
+		return this.brthdy;
+	}
+
+	public void setEml(String eml) {
+		this.eml = eml;
+	}
+
+	public String getEml() {
+		return this.eml;
+	}
+
+	public void setPhn(String phn) {
+		this.phn = phn;
+	}
+
+	public String getPhn() {
+		return this.phn;
+	}
+
+	public void setPstnPrd(int pstnPrd) {
+		this.pstnPrd = pstnPrd;
+	}
+
+	public int getPstnPrd() {
+		return this.pstnPrd;
+	}
+
+	public void setPrflPht(String prflPht) {
+		this.prflPht = prflPht;
+	}
+
+	public String getPrflPht() {
+		return this.prflPht;
+	}
+
+	public void setHrDt(String hrDt) {
+		this.hrDt = hrDt;
+	}
+
+	public String getHrDt() {
+		return this.hrDt;
+	}
+
+	public void setHrPrd(int hrPrd) {
+		this.hrPrd = hrPrd;
+	}
+
+	public int getHrPrd() {
+		return this.hrPrd;
+	}
+
+	public void setAddrss(String addrss) {
+		this.addrss = addrss;
+	}
+
+	public String getAddrss() {
+		return this.addrss;
+	}
+
+	public void setEmplmntStts(String emplmntStts) {
+		this.emplmntStts = emplmntStts;
+	}
+
+	public String getEmplmntStts() {
+		return this.emplmntStts;
+	}
+
+	public void setAdmnYn(String admnYn) {
+		this.admnYn = admnYn;
+	}
+
+	public String getAdmnYn() {
+		return this.admnYn;
+	}
+
+	public void setPwdChngDt(String pwdChngDt) {
+		this.pwdChngDt = pwdChngDt;
+	}
+
+	public String getPwdChngDt() {
+		return this.pwdChngDt;
+	}
+
+	public void setLtstLgnIp(String ltstLgnIp) {
+		this.ltstLgnIp = ltstLgnIp;
+	}
+
+	public String getLtstLgnIp() {
+		return this.ltstLgnIp;
+	}
+
+	public void setLtstLgnDt(String ltstLgnDt) {
+		this.ltstLgnDt = ltstLgnDt;
+	}
+
+	public String getLtstLgnDt() {
+		return this.ltstLgnDt;
+	}
+
+	public void setLgnCnt(int lgnCnt) {
+		this.lgnCnt = lgnCnt;
+	}
+
+	public int getLgnCnt() {
+		return this.lgnCnt;
+	}
+
+	public void setPwdSalt(String pwdSalt) {
+		this.pwdSalt = pwdSalt;
+	}
+
+	public String getPwdSalt() {
+		return this.pwdSalt;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getPwd() {
+		return this.pwd;
+	}
+
+	public void setPstnId(int pstnId) {
+		this.pstnId = pstnId;
+	}
+
+	public int getPstnId() {
+		return this.pstnId;
+	}
+
+	public void setJobId(int jobId) {
+		this.jobId = jobId;
+	}
+
+	public int getJobId() {
+		return this.jobId;
+	}
+
+	public void setDepId(String depId) {
+		this.depId = depId;
+	}
+
+	public String getDepId() {
+		return this.depId;
+	}
+
+}
---
+++ k-pms/src/main/java/com/kpms/lgntrylog/vo/LgnTryLogVO.java
@@ -0,0 +1,126 @@
+package com.kpms.lgntrylog.vo;
+
+/**
+ * LGN_TRY_LOG
+ */
+public class LgnTryLogVO {
+
+	/**
+	 * 
+	 */
+	private String lgnTryIp;
+
+	/**
+	 * 
+	 */
+	private String crtDt;
+
+	/**
+	 * 
+	 */
+	private String crtr;
+
+	/**
+	 * 
+	 */
+	private String mdfyDt;
+
+	/**
+	 * 
+	 */
+	private String mdfyr;
+
+	/**
+	 * 
+	 */
+	private String useYn;
+
+	/**
+	 * 
+	 */
+	private String delYn;
+
+	/**
+	 * 
+	 */
+	private int lgnTryCnt;
+
+	/**
+	 * 
+	 */
+	private String ltstLgnTryDt;
+
+
+	public void setLgnTryIp(String lgnTryIp) {
+		this.lgnTryIp = lgnTryIp;
+	}
+
+	public String getLgnTryIp() {
+		return this.lgnTryIp;
+	}
+
+	public void setCrtDt(String crtDt) {
+		this.crtDt = crtDt;
+	}
+
+	public String getCrtDt() {
+		return this.crtDt;
+	}
+
+	public void setCrtr(String crtr) {
+		this.crtr = crtr;
+	}
+
+	public String getCrtr() {
+		return this.crtr;
+	}
+
+	public void setMdfyDt(String mdfyDt) {
+		this.mdfyDt = mdfyDt;
+	}
+
+	public String getMdfyDt() {
+		return this.mdfyDt;
+	}
+
+	public void setMdfyr(String mdfyr) {
+		this.mdfyr = mdfyr;
+	}
+
+	public String getMdfyr() {
+		return this.mdfyr;
+	}
+
+	public void setUseYn(String useYn) {
+		this.useYn = useYn;
+	}
+
+	public String getUseYn() {
+		return this.useYn;
+	}
+
+	public void setDelYn(String delYn) {
+		this.delYn = delYn;
+	}
+
+	public String getDelYn() {
+		return this.delYn;
+	}
+
+	public void setLgnTryCnt(int lgnTryCnt) {
+		this.lgnTryCnt = lgnTryCnt;
+	}
+
+	public int getLgnTryCnt() {
+		return this.lgnTryCnt;
+	}
+
+	public void setLtstLgnTryDt(String ltstLgnTryDt) {
+		this.ltstLgnTryDt = ltstLgnTryDt;
+	}
+
+	public String getLtstLgnTryDt() {
+		return this.ltstLgnTryDt;
+	}
+
+}
