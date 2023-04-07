353a60c93f8d13e02e1da0b9ac57ae3735df9e74
JinbaeByeon <quswlsqo@gmail.com>
2023-04-07 17:11:23 +0900

    dbconfig 수정, 로그인 페이지 ui 적용

--- k-pms/src/main/java/com/kpms/emp/web/EmpController.java
+++ k-pms/src/main/java/com/kpms/emp/web/EmpController.java
@@ -14,7 +14,6 @@ public class EmpController {
 	
 	@GetMapping("/")
 	public String viewLgnPage() {
-		
 		return "emp/lgn";
 	}
 }
--- k-pms/src/main/java/com/kpms/emp/web/RestEmpController.java
+++ k-pms/src/main/java/com/kpms/emp/web/RestEmpController.java
@@ -1,13 +1,41 @@
 package com.kpms.emp.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kpms.common.api.vo.APIResponseVO;
+import com.kpms.common.api.vo.APIStatus;
+import com.kpms.common.exception.APIArgsException;
+import com.kpms.common.util.StringUtil;
 import com.kpms.emp.service.EmpService;
+import com.kpms.emp.vo.EmpVO;
 
 @RestController
 public class RestEmpController {
 
 	@Autowired
 	private EmpService empService;
+	
+	@PostMapping("/")
+	public APIResponseVO doCreateEmp(EmpVO empVO,String cPwd) {
+
+		if(StringUtil.isEmpty(empVO.getfNm())) {
+			throw new APIArgsException("404", "이름은 필수 값입니다.");
+		}
+		if(StringUtil.isEmpty(empVO.getEmpId())) {
+			throw new APIArgsException("404", "ID는 필수 값입니다.");
+		}
+		if(StringUtil.isEmpty(empVO.getEml())) {
+			throw new APIArgsException("404", "이메일은 필수 값입니다.");
+		}
+		if(StringUtil.isEmpty(empVO.getPwd())) {
+			throw new APIArgsException("404", "비밀번호는 필수 값입니다.");
+		}
+		
+		if(empService.createOneEmp(empVO)) {
+			return new APIResponseVO(APIStatus.OK);
+		}
+		return new APIResponseVO(APIStatus.FAIL);
+	}
 }
