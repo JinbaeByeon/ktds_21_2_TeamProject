6a84a0430100f9920be68784939f345f6e0c2803
JinbaeByeon <quswlsqo@gmail.com>
2023-04-17 20:28:29 +0900

    사원 상세정보 진행중

--- k-pms/src/main/java/com/kpms/emp/web/EmpController.java
+++ k-pms/src/main/java/com/kpms/emp/web/EmpController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kpms.common.handler.SessionHandler;
 import com.kpms.emp.service.EmpService;
@@ -37,6 +40,13 @@ public class EmpController {
 		session.invalidate();
 		empService.createLgnHst(lgnHst);
 		return "redirect:/";
+	}
+	
+	@GetMapping("/emp/detail/{empId}")
+	public String viewEmpDetailPage(@PathVariable String empId, Model model) {
+		EmpVO empVO = empService.readOneEmpByEmpId(empId);
+		model.addAttribute("empVO", empVO);
+		return "emp/detail";
 	}
 	
 	@GetMapping("/emp/list")
